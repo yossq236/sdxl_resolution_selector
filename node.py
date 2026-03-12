58dc7cb0b3d7d1bd1655fa6d74d53b38d263876b
yossq236 <yf0659@gmail.com>
2026-03-13 08:57:30 +0900

    fix formatting

--- node.py
+++ node.py
@@ -9,27 +9,32 @@ class Resolution:
         self.label = "{}x{}({}:{})".format(self.width, self.height, int(self.width // gcd), int(self.height // gcd))
 
 RESOLUTIONS = [
-    Resolution(1024,1024),
-    Resolution(960,1024),
-    Resolution(960,1088),
-    Resolution(896,1088),
-    Resolution(896,1152),
-    Resolution(832,1152),
-    Resolution(832,1216),
-    Resolution(768,1280),
-    Resolution(768,1344),
-    Resolution(704,1344),
-    Resolution(704,1408),
-    Resolution(704,1472),
-    Resolution(640,1536),
-    Resolution(640,1600),
-    Resolution(576,1664),
-    Resolution(576,1728),
-    Resolution(576,1792),
-    Resolution(512,1856),
-    Resolution(512,1920),
-    Resolution(512,1984),
-    Resolution(512,2048),
+    Resolution(1024, 1024),
+    Resolution( 960, 1024),
+    Resolution( 960, 1088),
+    Resolution( 896, 1088),
+    Resolution( 896, 1152),
+    Resolution( 832, 1152),
+    Resolution( 832, 1216),
+    Resolution( 768, 1280),
+    Resolution( 768, 1344),
+    Resolution( 704, 1344),
+    Resolution( 704, 1408),
+    Resolution( 704, 1472),
+    Resolution( 640, 1536),
+    Resolution( 640, 1600),
+    Resolution( 576, 1664),
+    Resolution( 576, 1728),
+    Resolution( 576, 1792),
+    Resolution( 512, 1856),
+    Resolution( 512, 1920),
+    Resolution( 512, 1984),
+    Resolution( 512, 2048),
+    ]
+
+ORIENTATIONS = [
+    "portrait",
+    "landscape",
     ]
 
 class SDXLResolutionSelectorNode(io.ComfyNode):
@@ -41,7 +46,7 @@ class SDXLResolutionSelectorNode(io.ComfyNode):
             category="utils",
             inputs=[
                 io.Combo.Input("resolution",options=[n.label for n in RESOLUTIONS],default=RESOLUTIONS[0].label),
-                io.Combo.Input("orientation",options=["portrait","landscape"],default="portrait"),
+                io.Combo.Input("orientation",options=ORIENTATIONS,default=ORIENTATIONS[0]),
                 io.Float.Input("scale_by",default=1.0,min=0.5,max=9.5),
                 ],
             outputs=[
@@ -61,8 +66,8 @@ class SDXLResolutionSelectorNode(io.ComfyNode):
         else:
             base_w = 1024
             base_h = 1024
-        scaled_w = int(((base_w * scale_by + 7) // 8) * 8)
-        scaled_h = int(((base_h * scale_by + 7) // 8) * 8)
+        scaled_w = int((base_w * scale_by + 7) // 8 * 8)
+        scaled_h = int((base_h * scale_by + 7) // 8 * 8)
         if orientation == "portrait":
             return io.NodeOutput(scaled_w, scaled_h, base_w, base_h)
         else:
