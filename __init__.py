d8136740c6b2abc3a8372b98215c187cbf9f919b
yossq236 <yf0659@gmail.com>
2026-04-06 00:54:11 +0900

    Remove redundant test code

--- __init__.py
+++ __init__.py
@@ -1,12 +1,11 @@
 from typing import override
 from comfy_api.latest import ComfyExtension, io
 from .node import SDXLResolutionSelectorNode
-from .node2 import SDXLCLIPTextEncodeNode
 
 class MyExtension(ComfyExtension):
     @override
     async def get_node_list(self) -> list[type[io.ComfyNode]]:
-        return [SDXLResolutionSelectorNode, SDXLCLIPTextEncodeNode]
+        return [SDXLResolutionSelectorNode]
     @override
     async def on_load(self):
         pass
